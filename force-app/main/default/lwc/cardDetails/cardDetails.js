41f939aeb8da728bf76ce8a2b2126e7f23de711d
Thélio Freitas <marquesthelio@hotmail.com>
2021-10-26 01:58:11 -0300

    Card details component

---
+++ force-app/main/default/lwc/cardDetails/cardDetails.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+
+export default class CardDetails extends LightningElement {
+  image = 'https://storage.googleapis.com/ygoprodeck.com/pics/6368038.jpg';
+  description = 'A knight whose horse travels faster than the wind. His battle-charge is a force to be reckogned with.';
+}
